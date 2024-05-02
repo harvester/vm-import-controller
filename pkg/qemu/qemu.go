bb1cef97a5cc02457caa2868c633bd0360dbf974
Gaurav Mehta <gaurav.mehta@suse.com>
2024-05-02 11:39:35 +1000

    added debug logging and also change behaviour of controller when vm export from source fails to stop rather than keep trying
    added more debug info
    
    close lease earlier
    
    stage vmware migration improvements
    
    improve openstack migration to check uefi/tpm/secureboot from associated image
    
    improve firmware lookup for openstack, and add ability to specify custom storage classes
    
    fix storage class caching issue
    
    drop need for sync
    
    fix imports

--- pkg/qemu/qemu.go
+++ pkg/qemu/qemu.go
@@ -1,27 +1,56 @@
 package qemu
 
 import (
+	"fmt"
+	"io"
 	"os/exec"
 	"syscall"
+
+	"github.com/sirupsen/logrus"
 )
 
 const defaultCommand = "qemu-wrapper.sh"
 
 func ConvertVMDKtoRAW(source, target string) error {
+	logrus.Infof("converting image %s to %s", source, target)
 	args := []string{"convert", "-f", "vmdk", "-O", "raw", source, target}
-	cmd := exec.Command(defaultCommand, args...)
-	cmd.SysProcAttr = &syscall.SysProcAttr{}
-	return cmd.Run()
+	return runCommand(defaultCommand, args...)
 }
 
 func ConvertQCOW2toRAW(source, target string) error {
 	args := []string{"convert", "-f", "qcow2", "-O", "raw", source, target}
-	cmd := exec.Command(defaultCommand, args...)
-	return cmd.Run()
+	return runCommand(defaultCommand, args...)
 }
 
 func createVMDK(path string, size string) error {
 	args := []string{"create", "-f", "vmdk", path, size}
-	cmd := exec.Command(defaultCommand, args...)
-	return cmd.Run()
+	return runCommand(defaultCommand, args...)
+}
+
+func runCommand(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	stderr, err := cmd.StderrPipe()
+
+	if err != nil {
+		return fmt.Errorf("error creating stderr pipe: %v", err)
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("error creating stdout pipe: %v", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("error in command start: %v", err)
+	}
+
+	errOut, _ := io.ReadAll(stderr)
+	out, err := io.ReadAll(stdout)
+	err = cmd.Wait()
+	if err != nil {
+		return fmt.Errorf("error in command: %s, %s", command, errOut)
+	}
+	logrus.Debugf("image command complete: %v", string(out))
+	return nil
 }
