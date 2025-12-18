45fb501cd0f1db31211f7e9a8a268e7fa490c191
Davide Rutigliano <davide.rutigliano@suse.com>
2025-12-18 22:07:46 +0100

    Add support for KVM CPU model
    Signed-off-by: Davide Rutigliano <davide.rutigliano@suse.com>

--- pkg/source/kvm/client_test.go
+++ pkg/source/kvm/client_test.go
@@ -17,9 +17,12 @@ func TestParseDomainXML(t *testing.T) {
   <memory unit='KiB'>4194304</memory>
   <vcpu placement='static'>2</vcpu>
   <os>
-    <type arch='x86_64' machine='pc-q35-6.2'>hvm</type>
+    <type arch='x86_64'>hvm</type>
     <boot dev='hd'/>
   </os>
+  <cpu mode='custom'>
+    <model>IvyBridge-IBRS</model>
+  </cpu>
   <devices>
     <disk type='file' device='disk'>
       <driver name='qemu' type='qcow2'/>
@@ -42,13 +45,14 @@ func TestParseDomainXML(t *testing.T) {
 	assert.Equal(t, "test-vm", dom.Name)
 	assert.Equal(t, "a75aca4b-42f6-4447-9262-4b9562d3d95c", dom.UUID)
 	assert.Equal(t, uint(4194304), dom.Memory.Value)
-	assert.Equal(t, 2, dom.VCPU.Value)
+	assert.Equal(t, uint(2), dom.VCPU.Value)
 	assert.Len(t, dom.Devices.Disks, 1)
 	assert.Equal(t, "/var/lib/libvirt/images/test-vm.qcow2", dom.Devices.Disks[0].Source.File.File)
 	assert.Equal(t, "qcow2", dom.Devices.Disks[0].Driver.Type)
 	assert.Len(t, dom.Devices.Interfaces, 1)
 	assert.Equal(t, "52:54:00:6b:3c:58", dom.Devices.Interfaces[0].MAC.Address)
 	assert.Equal(t, "default", dom.Devices.Interfaces[0].Source.Network.Network)
+	assert.Equal(t, "IvyBridge-IBRS", dom.CPU.Model.Value)
 }
 
 func TestNewClient(t *testing.T) {
